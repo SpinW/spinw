d1bb52d8c4fe7d60ea25cb4ff29c27733d9ab996
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-05-16 11:04:44 +0100

    Add coverage report for subfolders

--- run_tests.m
+++ run_tests.m
@@ -29,7 +29,9 @@ function result = run_tests(out_dir)
     cov_dirs = {'swfiles', 'external'};
     for i = 1:length(cov_dirs)
         reportFormat = CoberturaFormat(fullfile(out_dir, ['coverage_', cov_dirs{i}, '.xml']));
-        coverage_plugin = CodeCoveragePlugin.forFolder(cov_dirs{i}, 'Producing', reportFormat);
+        coverage_plugin = CodeCoveragePlugin.forFolder(cov_dirs{i}, ...
+                                                       'Producing', reportFormat, ...
+                                                       'IncludingSubfolders', true);
         runner.addPlugin(coverage_plugin);
         if verLessThan('matlab', '9.12') % Can add cov for multiple folders only from R2022a
             break;
