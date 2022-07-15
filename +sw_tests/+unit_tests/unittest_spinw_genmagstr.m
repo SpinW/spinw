4ef8e4c13f63c253194c2474e13b61e5a4dd575e
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-07-15 09:40:49 +0100

    Check mag_str not magstr

--- +sw_tests/+unit_tests/unittest_spinw_genmagstr.m
+++ +sw_tests/+unit_tests/unittest_spinw_genmagstr.m
@@ -3,11 +3,9 @@ classdef unittest_spinw_genmagstr < sw_tests.unit_tests.unittest_super
     properties
         swobj = [];
         swobj_tri = [];
-        default_magstr = struct('S', [0; 1; 0], ...
-                                'k', [0 0 0], ...
-                                'n', [0 0 1], ...
-                                'N_ext', [1 1 1], ...
-                                'exact', true);
+        default_mag_str = struct('nExt', ones(1, 3, 'int32'), ...
+                                 'k', [0; 0; 0], ...
+                                 'F', [0; 1; 0]);
     end
     
     methods (TestMethodSetup)
@@ -39,23 +37,24 @@ classdef unittest_spinw_genmagstr < sw_tests.unit_tests.unittest_super
         end
         function test_direct_fm_chain(testCase)
             swobj = copy(testCase.swobj);
-            swobj.genmagstr('mode','direct', 'k',[0 0 0],'n',[1 0 0],'S',[0; 1; 0]);
-            testCase.verify_obj(testCase.default_magstr, swobj.magstr);
+            swobj.genmagstr('mode', 'direct', 'k', [0 0 0], ...
+                            'n', [1 0 0], 'S', [0; 1; 0]);
+            testCase.verify_obj(testCase.default_mag_str, swobj.mag_str);
 
         end
         function test_direct_fm_chain_nok(testCase)
             swobj = copy(testCase.swobj);
-            swobj.genmagstr('mode','direct', 'n',[1 0 0],'S',[0; 1; 0]);
-            testCase.verify_obj(testCase.default_magstr, swobj.magstr);
+            swobj.genmagstr('mode', 'direct', 'n', [1 0 0], 'S', [0; 1; 0]);
+            testCase.verify_obj(testCase.default_mag_str, swobj.mag_str);
         end
         function test_helical_tri(testCase)
             swobj_tri = copy(testCase.swobj_tri);
             swobj_tri.genmagstr('mode', 'helical', 'S', [1; 0; 0], ...
-                                         'n', [0 0 1], 'k', [1/3 1/3 0]);
-            expected_magstr = testCase.default_magstr;
-            expected_magstr.S = [1.5; 0; 0];
-            expected_magstr.k = [1/3 1/3 0];
-            testCase.verify_obj(expected_magstr, swobj_tri.magstr);
+                                'n', [0 0 1], 'k', [1/3 1/3 0]);
+            expected_mag_str = testCase.default_mag_str;
+            expected_mag_str.k = [1/3; 1/3; 0];
+            expected_mag_str.F = [1.5; 1.5j; 0];
+            testCase.verify_obj(expected_mag_str, swobj_tri.mag_str);
 
         end
         function test_direct_afm_chain(testCase)
@@ -63,45 +62,48 @@ classdef unittest_spinw_genmagstr < sw_tests.unit_tests.unittest_super
             afm_chain.genmagstr('mode', 'direct', 'k',[1/2 0 0], ...
                                 'n',[1 0 0],'S',[0 0; 1 -1;0 0], ...
                                 'nExt',[2 1 1]);
-            expected_magstr = testCase.default_magstr;
-            expected_magstr.S = [0 0; 1 -1; 0 0];
-            expected_magstr.N_ext = [2 1 1];
-            testCase.verify_obj(expected_magstr, afm_chain.magstr);
+            expected_mag_str = testCase.default_mag_str;
+            expected_mag_str.nExt(1) = int32(2);
+            expected_mag_str.k = [0.5; 0; 0];
+            expected_mag_str.F = [0 0; 1 -1; 0 0];
+            testCase.verify_obj(expected_mag_str, afm_chain.mag_str);
 
         end
         function test_random_structure(testCase)
             swobj = copy(testCase.swobj);
             swobj.genmagstr('mode','random');
-            magstr1 = swobj.magstr;
+            mag_str1 = swobj.mag_str;
             swobj.genmagstr('mode','random');
-            magstr2 = swobj.magstr;
-            % Check structure is random each time - S is different
-            testCase.verifyNotEqual(magstr1.S, magstr2.S);
+            mag_str2 = swobj.mag_str;
+            % Check structure is random each time - F is different
+            testCase.verifyNotEqual(mag_str1.F, mag_str2.F);
             % Check S has correct magnitude
-            testCase.verify_val(norm(magstr1.S, 2), 1);
+            testCase.verify_val(norm(swobj.magstr.S, 2), 1);
             % Check other fields
-            expected_magstr = testCase.default_magstr;
-            testCase.verify_obj(rmfield(expected_magstr, 'S'), ...
-                                rmfield(magstr1, 'S'));
+            expected_mag_str = testCase.default_mag_str;
+            testCase.verify_obj(rmfield(expected_mag_str, 'F'), ...
+                                rmfield(mag_str1, 'F'));
         end
         function test_random_structure_multiatom_and_nExt(testCase)
             swobj = copy(testCase.swobj);
-            swobj.addatom('r', [0.5 0.5 0],'S', 2);
-            nExt = [2 2 2];
-            swobj.genmagstr('mode','random', 'nExt', nExt);
-            magstr1 = swobj.magstr;
-            swobj.genmagstr('mode','random', 'nExt', nExt);
-            magstr2 = swobj.magstr;
-            % Check structure is random each time - S is different
-            testCase.verifyNotEqual(magstr1.S, magstr2.S);
+            swobj.addatom('r', [0.5 0.5 0], 'S', 2);
+            nExt = 2*ones(1, 3, 'int32');
+            swobj.genmagstr('mode', 'random', 'nExt', nExt);
+            mag_str1 = swobj.mag_str;
+            swobj.genmagstr('mode', 'random', 'nExt', nExt);
+            mag_str2 = swobj.mag_str;
+            % Check structure is random each time - F is different
+            testCase.verifyNotEqual(mag_str1.F, mag_str2.F);
             % Check S has correct magnitudes
-            testCase.verify_val(vecnorm(magstr1.S(:, 1:2:end), 2), ones(1, 8));
-            testCase.verify_val(vecnorm(magstr1.S(:, 2:2:end), 2), 2*ones(1, 8));
+            testCase.verify_val(vecnorm(swobj.magstr.S(:, 1:2:end), 2), ...
+                                ones(1, 8));
+            testCase.verify_val(vecnorm(swobj.magstr.S(:, 2:2:end), 2), ...
+                                2*ones(1, 8));
             % Check other fields
-            expected_magstr = testCase.default_magstr;
-            expected_magstr.N_ext = nExt;
-            testCase.verify_obj(rmfield(expected_magstr, 'S'), ...
-                                rmfield(magstr1, 'S'));
+            expected_mag_str = testCase.default_mag_str;
+            expected_mag_str.nExt = nExt;
+            testCase.verify_obj(rmfield(expected_mag_str, 'F'), ...
+                                rmfield(mag_str1, 'F'));
         end
     end
 
