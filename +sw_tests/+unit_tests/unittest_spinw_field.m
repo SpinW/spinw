5f3ddb368f396d9980486faaf78763eee6b09a5e
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-08-02 16:02:14 +0100

    Small changes from review comments
    - Create spinw obj in TestMethodSetup
    - Try multiple size incorrect inputs to trigger error
    - Change field return value test to check for handle

--- +sw_tests/+unit_tests/unittest_spinw_field.m
+++ +sw_tests/+unit_tests/unittest_spinw_field.m
@@ -1,59 +1,63 @@
 classdef unittest_spinw_field < sw_tests.unit_tests.unittest_super
+    properties
+        swobj
+    end
+    properties (TestParameter)
+        incorrect_input = {0, [1; 1], ones(1, 4)};
+    end
+    methods(TestMethodSetup)
+        function create_sw_model(testCase)
+            testCase.swobj = spinw();
+        end
+    end
     methods (Test)
-        function test_incorrect_shape_field_raises_error(testCase)
-            swobj = spinw();
+        function test_incorrect_shape_field_raises_error(testCase, ...
+                                                         incorrect_input)
             testCase.verifyError(...
-                @() field(swobj, [0; 0]), ...
+                @() field(testCase.swobj, incorrect_input), ...
                 'spinw:magfield:ArraySize');
         end
         function test_default_field(testCase)
-            swobj = spinw();
-            testCase.assertEqual(field(swobj), [0 0 0]);
+            testCase.assertEqual(field(testCase.swobj), [0 0 0]);
         end
         function test_set_field(testCase)
-            swobj = spinw();
             field_val = [1 2 3];
-            field(swobj, field_val);
-            testCase.assertEqual(field(swobj), field_val);
+            field(testCase.swobj, field_val);
+            testCase.assertEqual(field(testCase.swobj), field_val);
         end
         function test_set_field_column_vector(testCase)
-            swobj = spinw();
             field_val = [1; 2; 3];
-            field(swobj, field_val);
-            testCase.assertEqual(field(swobj), field_val');
+            field(testCase.swobj, field_val);
+            testCase.assertEqual(field(testCase.swobj), field_val');
         end
         function test_set_field_multiple_times(testCase)
-            swobj = spinw();
-            field(swobj, [1 2 3]);
+            field(testCase.swobj, [1 2 3]);
             new_field_val = [1.5 2.5 -3.5];
-            field(swobj, new_field_val);
-            testCase.assertEqual(field(swobj), new_field_val);
+            field(testCase.swobj, new_field_val);
+            testCase.assertEqual(field(testCase.swobj), new_field_val);
         end
         function test_returns_spinw_obj(testCase)
-            swobj = spinw();
             field_val = [1 2 3];
-            new_swobj = field(swobj, field_val);
+            new_swobj = field(testCase.swobj, field_val);
             % Test field is set
-            testCase.assertEqual(field(swobj), field_val)
-            % Also test swobj is returned
-            testCase.verify_obj(swobj, new_swobj);
+            testCase.assertEqual(field(testCase.swobj), field_val)
+            % Also test handle to swobj is returned
+            assert (testCase.swobj == new_swobj);
         end
     end
     methods (Test, TestTags = {'Symbolic'})
         function test_set_field_sym_mode_sym_input(testCase)
-            swobj = spinw();
-            swobj.symbolic(true);
+            testCase.swobj.symbolic(true);
             field_val = sym([pi pi/2 pi/3]);
-            field(swobj, field_val);
-            testCase.assertEqual(field(swobj), field_val);
+            field(testCase.swobj, field_val);
+            testCase.assertEqual(field(testCase.swobj), field_val);
         end
         function test_set_field_sym_mode_non_sym_input(testCase)
-            swobj = spinw();
-            swobj.symbolic(true);
+            testCase.swobj.symbolic(true);
             field_val = [pi pi/2 pi/3];
-            field(swobj, field_val);
+            field(testCase.swobj, field_val);
             expected_field_val = field_val*sym('B');
-            testCase.assertEqual(field(swobj), expected_field_val);
+            testCase.assertEqual(field(testCase.swobj), expected_field_val);
         end
     end
 end
