239271b04697bb524016ba27d397eabc811a2efb
Richard Waite <richard.waite@stfc.ac.uk>
2022-09-26 15:27:35 +0100

    Add tests for zero nExt and warning of self coupling & no convergence

--- +sw_tests/+unit_tests/unittest_spinw_optmagsteep.m
+++ +sw_tests/+unit_tests/unittest_spinw_optmagsteep.m
@@ -9,10 +9,10 @@ classdef unittest_spinw_optmagsteep < sw_tests.unit_tests.unittest_super
     properties (TestParameter)
         existing_plot = {true, false};
     end
-    methods (TestClassSetup)
+    methods (TestMethodSetup)
         function setup_afm_chain_model_easy_axis(testCase)            
             testCase.swobj = spinw();
-            testCase.swobj.genlattice('lat_const',[2 4 6])
+            testCase.swobj.genlattice('lat_const', [2 3 6])
             testCase.swobj.addatom('r',[0; 0; 0],'S',1)
             testCase.swobj.addmatrix('label', 'A', ...
                                      'value', diag([0 0 -0.1])) % c easy
@@ -24,11 +24,37 @@ classdef unittest_spinw_optmagsteep < sw_tests.unit_tests.unittest_super
     end
 
     methods (Test)
-        function test_invalid_nExt_throws_error(testCase)
+        function test_no_nExt_throws_error(testCase)
             testCase.swobj.genmagstr('mode', 'direct', 'S', [0; 0; 1], ...
-                                     'k',[0,0,0]);
+                                     'k',[0, 0, 0]);
             func_call = @() testCase.swobj.optmagsteep('random', true);
             testCase.verifyError(func_call, 'spinw:optmagsteep:NoField')
+        end
+        
+        function test_invalid_nExt_throws_error(testCase)
+            func_call = @() testCase.swobj.optmagsteep('nExt', [0, 0, 0]);
+            testCase.verifyError(func_call, 'spinw:optmagsteep:WrongInput')
+        end
+        
+        function test_warns_if_not_convereged(testCase)
+            % init moment along hard-axis (far from minima) and run 1 iter
+            testCase.swobj.genmagstr('mode', 'helical', 'S', [1; 0; 0], ...
+                                     'k',[0.5,0,0], 'n', [0,1,0], ...
+                                     'nExt', [2,1,1]);
+            func_call = @() testCase.swobj.optmagsteep('nRun', 1);
+            testCase.verifyWarning(func_call, ...
+                                   'spinw:optmagsteep:NotConverged')
+        end
+        
+        function test_warns_of_self_coupling_spins(testCase)
+            % add AFM exchange to bond along b (will double cell along b)
+            testCase.swobj.addmatrix('label', 'J2', 'value', 0.25)  % AFM
+            testCase.swobj.addcoupling('mat', 'J2', 'bond', 2); % along b
+            % run opt with k only along a
+            func_call = @() testCase.swobj.optmagsteep('NExt', [2, 1, 1], ...
+                                                       'nRun', 1);
+            testCase.verifyWarning(func_call, ...
+                                   'spinw:optmagsteep:SelfCoupling')
         end
         
         function test_starting_from_groundstate_executes_one_iteration(testCase)
