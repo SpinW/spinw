d40208ce8aa7610c97de25093c95a663ffe51c71
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-10-27 16:13:43 +0100

    Small changes:
    - Use TestMethodSetup so a new spinw object is used in each test
    - Put default 'F' in properties

--- +sw_tests/+unit_tests/unittest_spinw_optmagk.m
+++ +sw_tests/+unit_tests/unittest_spinw_optmagk.m
@@ -3,12 +3,14 @@ classdef unittest_spinw_optmagk < sw_tests.unit_tests.unittest_super
 
     properties
         swobj = [];
-        swobj_tri = [];
         % output from optmagk
         default_mag_str = struct('nExt', int32([1 1 1]), ...
+                                 'F', [sqrt(1/3) + 1i*sqrt(1/2); ...
+                                       sqrt(1/3); ...
+                                       sqrt(1/3) - 1i*sqrt(1/2)], ...
                                  'k', [1; 0; 0]);
     end
-    methods (TestClassSetup)
+    methods (TestMethodSetup)
         function setup_chain_model(testCase)            
             testCase.swobj = spinw();
             testCase.swobj.genlattice('lat_const', [3 8 8])
@@ -17,40 +19,31 @@ classdef unittest_spinw_optmagk < sw_tests.unit_tests.unittest_super
         end
     end
     methods (Test)
-        function test_fm_opt(testCase)
+        function test_fm_optk(testCase)
             testCase.swobj.addmatrix('label', 'J1', 'value', -1);
             testCase.swobj.addcoupling('mat', 'J1', 'bond', 1);
             out = testCase.swobj.optmagk;
             out.stat = rmfield(out.stat, 'nFunEvals');
-            k = [1; 0; 0];
-            F = [sqrt(1/3) + 1i*sqrt(1/2); ...
-                 sqrt(1/3); ...
-                 sqrt(1/3) - 1i*sqrt(1/2)];
-            expected_out = struct('k', k, ...
+
+            expected_mag_str = testCase.default_mag_str;
+            expected_out = struct('k', expected_mag_str.k, ...
                                   'E', -1, ....
-                                  'F', F, ...
+                                  'F', expected_mag_str.F, ...
                                   'stat', struct('S', 0, ...
                                                  'exitflag', -1));
             % Test struct output by optmagk
             testCase.verify_val(out, expected_out, 'abs_tol', 1e-4);
             % Also test spinw attributes have been set
             expected_mag_str = testCase.default_mag_str;
-            expected_mag_str.F = F;
             testCase.verify_val(testCase.swobj.mag_str, expected_mag_str, ...
                                 'abs_tol', 1e-4);
         end
-    end
-    methods (Test)
-        function test_afm_opt(testCase)
+        function test_afm_optk(testCase)
             testCase.swobj.addmatrix('label', 'J1', 'value', 1);
             testCase.swobj.addcoupling('mat', 'J1', 'bond', 1);
             testCase.swobj.optmagk;
-            % Also test spinw attributes have been set
             expected_mag_str = testCase.default_mag_str;
             expected_mag_str.k = [0.5; 0; 0];
-            expected_mag_str.F = [sqrt(1/3) + 1i*sqrt(1/2); ...
-                                  sqrt(1/3); ...
-                                  sqrt(1/3) - 1i*sqrt(1/2)];
             testCase.verify_val(testCase.swobj.mag_str, expected_mag_str, ...
                                 'abs_tol', 1e-4);
         end
