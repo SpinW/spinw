32c40472cc0b97307a6c7fefbffc33ee27019f25
Richard Waite <richard.waite@stfc.ac.uk>
2024-04-30 16:59:35 +0100

    Add unit tests for sw_fitpowder

---
+++ +sw_tests/+unit_tests/unittest_sw_fitpowder.m
@@ -0,0 +1,243 @@
+classdef unittest_sw_fitpowder < sw_tests.unit_tests.unittest_super
+    % Runs through unit test for @spinw/spinwave.m
+
+    properties
+        swobj = [];
+        data_1d_cuts = arrayfun(@(qmin) struct('x', 1:3, 'y', 1:3, ...
+                                               'e', 1:3, 'qmin', qmin, ...
+                                               'qmax',qmin+1), 3.5:4.5);
+        data_2d = struct('x', {{1:3, 4:5}}, 'y', [1:3; 1:3], ...
+                         'e', [1:3; 1:3]);
+        fit_func =  @(obj, p) matparser(obj, 'param', p, 'mat', {'J_1'}, 'init', true);
+        j1 = 2;
+        default_fitpow = [];
+        default_fields = [];
+    end
+
+    methods (TestClassSetup)
+        function setup_spinw_obj_and_expected_result(testCase)
+            % setup spinw object
+            testCase.swobj = sw_model('triAF', 1);
+            % subset of default fitpow object fields
+            testCase.default_fitpow = struct('y', testCase.data_2d.y', ...
+                                             'e', testCase.data_2d.e', ...
+                                             'ebin_cens', testCase.data_2d.x{1}, ...
+                                             'modQ_cens', testCase.data_2d.x{2}, ...
+                                             'params', [2;0;0;0;1], ...
+                                             'bounds', [-Inf   Inf;
+                                                        -Inf   Inf;
+                                                        -Inf   Inf;
+                                                        -Inf   Inf;
+                                                           0   Inf]);
+            testCase.default_fields = fieldnames(testCase.default_fitpow);
+        end
+    end
+
+    methods
+        function verify_results(testCase, observed, expected, fieldnames)
+            if nargin < 4
+                fieldnames = testCase.default_fields;
+            end
+            for ifld = 1:numel(fieldnames)
+                fld = fieldnames{ifld};
+                testCase.verify_val(observed.(fld), expected.(fld));
+            end
+        end
+    end
+
+    methods (Test)
+        function test_init_data_2d(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            testCase.verify_results(out, testCase.default_fitpow);
+        end
+
+        function test_init_data_1d_planar_bg(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.modQ_cens = 3.55:0.1:5.45; % integrtate over nQ pts
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_init_data_1d_indep_bg(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent");
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.modQ_cens = 3.55:0.1:5.45; % integrtate over nQ pts
+            % add extra background param
+            expected_fitpow.params = expected_fitpow.params([1:2,2:end],:);
+            expected_fitpow.bounds = expected_fitpow.bounds([1:2,2:end],:);
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_init_data_1d_specify_nQ(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "planar", 1);
+            testCase.verify_results(out, testCase.default_fitpow);
+        end
+
+        function test_set_model_parameters(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, 5);
+            out.set_model_parameters(1, testCase.j1);
+            testCase.verify_results(out, testCase.default_fitpow);
+        end
+
+        function test_set_model_parameter_bounds(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            lb = -5;
+            ub = 5;
+            out.set_model_parameter_bounds(1, -5, 5);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.bounds(1, :) = [lb, ub];
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_fix_model_parameters(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            out.fix_model_parameters(1);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.bounds(1, :) = [testCase.j1, testCase.j1];
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_bg_parameters_planar_bg(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            bg_pars = [-5, 5];
+            out.set_bg_parameters(1:2, bg_pars);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params(2:3) = bg_pars;
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_bg_parameters_indep_bg_all_cuts(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            bg_pars = [-5, 5];
+            out.set_bg_parameters(1:2, bg_pars);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = [expected_fitpow.params(1);
+                                      bg_pars(:); bg_pars(:); 1];
+            expected_fitpow.bounds = expected_fitpow.bounds([1:2,2:end],:);
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_bg_parameters_indep_bg_specify_icut(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            bg_pars = [-5, 5];
+            out.set_bg_parameters(1:2, bg_pars, 2);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = [expected_fitpow.params(1:3);
+                                      bg_pars(:); 1];
+            expected_fitpow.bounds = expected_fitpow.bounds([1:2,2:end],:);
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_fix_bg_parameters_indep_bg_specify_icut(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            out.fix_bg_parameters(1:2, 2);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = expected_fitpow.params([1:2,2:end],:);
+            expected_fitpow.bounds = [expected_fitpow.bounds(1:3,:);
+                                      zeros(2,2);
+                                      expected_fitpow.bounds(end,:)];
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_fix_bg_parameters_indep_bg_all_cuts(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            out.fix_bg_parameters(1:2);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = expected_fitpow.params([1:2,2:end],:);
+            expected_fitpow.bounds = [expected_fitpow.bounds(1,:);
+                                      zeros(4,2);
+                                      expected_fitpow.bounds(end,:)];
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_bg_parameter_bounds_indep_bg_all_cuts(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            ub = 5;
+            out.set_bg_parameter_bounds(1:2, [], [ub, ub]); % lb unchanged
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = expected_fitpow.params([1:2,2:end]);
+            expected_fitpow.bounds = expected_fitpow.bounds([1:2,2:end],:);
+            expected_fitpow.bounds(2:5, 2) = ub;
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_bg_parameter_bounds_indep_bg_specify_icut(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_1d_cuts, ...
+                               testCase.fit_func, testCase.j1, "independent", 1);
+            ub = 5;
+            out.set_bg_parameter_bounds(1:2, [], [ub, ub], 1); % lb unchanged
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params = expected_fitpow.params([1:2,2:end]);
+            expected_fitpow.bounds = expected_fitpow.bounds([1:2,2:end],:);
+            expected_fitpow.bounds(2:3, 2) = ub;
+            testCase.verify_results(out, expected_fitpow);
+        end
+        
+        function test_set_scale(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            scale = 5;
+            out.set_scale(scale);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.params(end) = scale;
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_fix_scale(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            out.fix_scale();
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.bounds(end, :) = 1;
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_set_scale_bounds(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            lb = 0.5;
+            out.set_scale_bounds(0.5, []);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.bounds(end, 1) = lb;
+            testCase.verify_results(out, expected_fitpow);
+        end
+
+        function test_crop_energy_range(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            out.crop_energy_range(2,5);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.y = expected_fitpow.y(2:end,:);
+            expected_fitpow.e = expected_fitpow.e(2:end,:);
+            expected_fitpow.ebin_cens = expected_fitpow.ebin_cens(2:end);
+            testCase.verify_results(out, expected_fitpow);
+            testCase.verify_val(out.powspec_args.Evect, ...
+                                expected_fitpow.ebin_cens)
+        end
+
+        function test_crop_q_range(testCase)
+            out = sw_fitpowder(testCase.swobj, testCase.data_2d, ...
+                               testCase.fit_func, testCase.j1);
+            out.crop_q_range(4.5,5.5);
+            expected_fitpow = testCase.default_fitpow;
+            expected_fitpow.y = expected_fitpow.y(:, 2:end);
+            expected_fitpow.e = expected_fitpow.e(:, 2:end);
+            expected_fitpow.modQ_cens = expected_fitpow.modQ_cens(2:end);
+            testCase.verify_results(out, expected_fitpow);
+        end
+    end
+
+end
