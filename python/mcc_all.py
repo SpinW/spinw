4795d097bcad9f2be729bed3054a390dc151770e
Duc Le <duc.le@stfc.ac.uk>
2024-05-31 14:12:14 +0100

    Change compile ctf CI to use mcc_all.py

--- python/mcc_all.py
+++ python/mcc_all.py
@@ -2,7 +2,7 @@ from libpymcr.utils import checkPath
 import os
 import subprocess
 
-for v in ['R2021a', 'R2021b', 'R2022a', 'R2022b', 'R2023a', 'R2023b', 'R2023a']:
+for v in ['R2021a', 'R2021b', 'R2022a', 'R2022b', 'R2023a', 'R2023b', 'R2024a']:
     print(f'Compiling for {v}')
     mlPath = checkPath(v)
     rv = subprocess.run([os.path.join(mlPath, 'bin', 'matlab'), '-batch', '"build_ctf; exit"'], capture_output=True)
